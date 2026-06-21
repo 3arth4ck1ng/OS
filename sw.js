da92cc0c1e1dd18f02cff22dc6f5632c498eac52
3arth4ck1ng <129807476+3arth4ck1ng@users.noreply.github.com>
2026-06-21 01:06:00 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='gerador-os-v1';
-const URLS=['./','./index.html'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
